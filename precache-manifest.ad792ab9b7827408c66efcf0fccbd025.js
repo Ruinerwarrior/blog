d9ccf4b89989b5f37a30d9670ccffeb13198f0fe
Rick Nijhuis <rick.nijhuis@sycade.com>
2020-06-24 21:39:47 +0200

    Updates

---
+++ precache-manifest.ad792ab9b7827408c66efcf0fccbd025.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d43e0528c486a56e22dac9c2f0e141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aff6255502e529c1337",
+    "url": "/static/css/main.1b8ac635.chunk.css"
+  },
+  {
+    "revision": "9615a260acc5b77eecf5",
+    "url": "/static/js/2.126e9f88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.126e9f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aff6255502e529c1337",
+    "url": "/static/js/main.8e8ed9d4.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "953c074dd1a54572a1f6b2f861d2f0ee",
+    "url": "/static/media/background.953c074d.jpg"
+  },
+  {
+    "revision": "58bf6fc758813b37ddaacac516b8613e",
+    "url": "/static/media/cpp.58bf6fc7.png"
+  },
+  {
+    "revision": "1f8d2604426d7020f2cc73e51da459c2",
+    "url": "/static/media/csharp.1f8d2604.png"
+  },
+  {
+    "revision": "772175ff1df43fd3c0bd3b594574cd68",
+    "url": "/static/media/js.772175ff.png"
+  }
+]);
