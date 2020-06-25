63aa48eb044e747b7045a7d7c4fcfb928b8cdbbc
Rick Nijhuis <rick.nijhuis@sycade.com>
2020-06-25 07:57:58 +0200

    fix build

--- src/Api.tsx
+++ src/Api.tsx
@@ -7,7 +7,8 @@ const headers = {
 }
 
 async function getAll<T>(route: string): Promise<T> {
-  const response = await fetch(`http://localhost:3000/${route}`, { method: 'GET', headers });
+  console.log("test", process.env.REACT_APP_API_URL);
+  const response = await fetch(`${process.env.REACT_APP_API_URL}${route}`, { method: 'GET', headers });
 
   if (!response.ok)
     throw new Error(`HTTP error status: ${response.status}`);
