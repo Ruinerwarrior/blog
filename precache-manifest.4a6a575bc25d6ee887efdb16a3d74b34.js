d3b316fb56a345cf1be421ca91fe19dd92f031d1
Ruinerwarrior <40138649+Ruinerwarrior@users.noreply.github.com>
2020-06-25 07:35:08 +0000

    Deploying to gh-pages from  @ 00c2454e5ac29d23feea8e2ede384d19c37b638a 🚀

---
+++ precache-manifest.4a6a575bc25d6ee887efdb16a3d74b34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e237ffeb017a8aca29c6d6793a770029",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "d2db71618db17f946f9a",
+    "url": "/blog/static/css/main.1b8ac635.chunk.css"
+  },
+  {
+    "revision": "be186bffb1e8037a12b6",
+    "url": "/blog/static/js/2.c48c2a85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.c48c2a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2db71618db17f946f9a",
+    "url": "/blog/static/js/main.07f431ca.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  },
+  {
+    "revision": "953c074dd1a54572a1f6b2f861d2f0ee",
+    "url": "/blog/static/media/background.953c074d.jpg"
+  },
+  {
+    "revision": "58bf6fc758813b37ddaacac516b8613e",
+    "url": "/blog/static/media/cpp.58bf6fc7.png"
+  },
+  {
+    "revision": "1f8d2604426d7020f2cc73e51da459c2",
+    "url": "/blog/static/media/csharp.1f8d2604.png"
+  },
+  {
+    "revision": "772175ff1df43fd3c0bd3b594574cd68",
+    "url": "/blog/static/media/js.772175ff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.14133305ffaf062b644e4c65104d014f.js"
+  "/blog/precache-manifest.4a6a575bc25d6ee887efdb16a3d74b34.js"
 );
 
 self.addEventListener('message', (event) => {
