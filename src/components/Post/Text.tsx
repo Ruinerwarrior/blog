87f3dd9e6e38cf2b609eec579bddf178eac4af5f
Rick Nijhuis <rick.nijhuis@sycade.com>
2020-06-26 14:52:30 +0200

    fix default styling

--- src/components/Post/Text.tsx
+++ src/components/Post/Text.tsx
@@ -5,6 +5,20 @@ const Text = styled.div`
   grid-row-start: 3;
   grid-row-end: 4;
   overflow-y: hidden;
+  
+  pre {
+    border-radius: 5px;
+    padding: 1em;
+    background-color: #EFF0F1;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+
+  img {
+    max-width: 100%
+  }
 `;
 
 export default Text;
